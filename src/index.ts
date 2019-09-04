bed96eba0e38cfba2e43a55d2fadce4190ba2ce6
remew <foolishremew@gmail.com>
2019-09-04 12:53:00 +0900

    avoid anonymous function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-export default <T>(v: T): InitialMatcher<T> => {
+const twitch = <T>(v: T): InitialMatcher<T> => {
   return new InitialMatcherImpl(v);
 };
+export default twitch;
 
 export interface Matcher<ValueType, ResultValueType> {
   case<V extends ValueType, ReturnValueType2>(v: V, callback: () => ReturnValueType2): Matcher<ValueType, ResultValueType | ReturnValueType2>;
