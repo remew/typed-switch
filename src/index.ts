c9ba2a59334fb91bd4382a673f382ad005f67cbd
remew <foolishremew@gmail.com>
2019-09-06 00:48:54 +0900

    fix lint error

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,33 @@
-const xwitch = <T>(v: T): InitialMatcher<T> => {
-  return new InitialMatcherImpl(v);
-};
-export default xwitch;
-
 export interface Matcher<ValueType, ResultValueType> {
-  case<V extends ValueType, ReturnValueType2>(v: V, callback: () => ReturnValueType2): Matcher<ValueType, ResultValueType | ReturnValueType2>;
+  case<V extends ValueType, ReturnValueType2>(
+    v: V,
+    callback: () => ReturnValueType2
+  ): Matcher<ValueType, ResultValueType | ReturnValueType2>;
   end(): ResultValueType | undefined;
   default<DefaultType>(callback: () => DefaultType): ResultValueType | DefaultType;
 }
 
 export interface InitialMatcher<ValueType> {
-  case<V extends ValueType, ReturnValueType>(v: V, callback: () => ReturnValueType): Matcher<ValueType, ReturnValueType>;
+  case<V extends ValueType, ReturnValueType>(
+    v: V,
+    callback: () => ReturnValueType
+  ): Matcher<ValueType, ReturnValueType>;
 }
 
 class MatcherImpl<ValueType, ReturnType> implements Matcher<ValueType, ReturnType> {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   private map: Map<ValueType, () => any>;
 
-  constructor(private targetValue: ValueType, v: any, callback: () => ReturnType) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  constructor(private targetValue: ValueType, firstValue: any, callback: () => ReturnType) {
     this.map = new Map();
-    this.map.set(v, callback);
+    this.map.set(firstValue, callback);
   }
 
-  case<V extends ValueType, NewReturnType>(v: V, callback: () => NewReturnType): Matcher<ValueType, ReturnType | NewReturnType> {
+  case<V extends ValueType, NewReturnType>(
+    v: V,
+    callback: () => NewReturnType
+  ): Matcher<ValueType, ReturnType | NewReturnType> {
     this.map.set(v, callback);
     return this;
   }
@@ -46,7 +52,15 @@ class MatcherImpl<ValueType, ReturnType> implements Matcher<ValueType, ReturnTyp
 class InitialMatcherImpl<ValueType> implements InitialMatcher<ValueType> {
   constructor(private target: ValueType) {}
 
-  case<V extends ValueType, ReturnValueType>(v: V, callback: () => ReturnValueType): Matcher<ValueType, ReturnValueType> {
+  case<V extends ValueType, ReturnValueType>(
+    v: V,
+    callback: () => ReturnValueType
+  ): Matcher<ValueType, ReturnValueType> {
     return new MatcherImpl(this.target, v, callback);
   }
 }
+
+const xwitch = <T>(v: T): InitialMatcher<T> => {
+  return new InitialMatcherImpl(v);
+};
+export default xwitch;
